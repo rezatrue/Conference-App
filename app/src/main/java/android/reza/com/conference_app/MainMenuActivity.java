a5e9621f796bf2ea44c327db72f6d4d8ab9fcb41
rezatrue <rezatrue@gmail.com>
2017-10-01 14:01:30 +0600

    30Sept

--- app/src/main/java/android/reza/com/conference_app/MainMenuActivity.java
+++ app/src/main/java/android/reza/com/conference_app/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package android.reza.com.conference_app;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,7 +15,12 @@ public class MainMenuActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        String[] items = getResources().getStringArray(R.array.menu_items);;
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+
+        String[] items = getResources().getStringArray(R.array.menu_items);
         menuRecycleView = (RecyclerView) findViewById(R.id.menuRecycleListView);
         menuAdapter = new MenuAdapter(this,items);
         LinearLayoutManager llm = new LinearLayoutManager(this);
